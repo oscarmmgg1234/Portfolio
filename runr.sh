ecca6e629eafab6f8fd471132336d38c5d2055cc
oscarmmgg1234 <45194139+oscarmmgg1234@users.noreply.github.com>
2020-09-27 08:17:16 -0700

    Create runr.sh

---
+++ runr.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#gives permision to run this script
+chmod +x runr.sh
+
+read -p 'Commit Message: ' COMMIT
+
+#git commit 
+git add .
+git commit -m "$COMMIT"
+git push origin master
+
+#build for firebase
+npm run build
+
+#firebase deploy
+firebase deploy --only hosting
+
+echo 'Script Done Boiii!'
