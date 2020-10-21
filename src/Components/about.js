30bf50adfbfd57bf1a7e54c5ffa591e3f3d5601f
Oscar Maldonado <oscarmmgg1234@gmail.com>
2020-10-20 20:49:52 -0700

    chartJsAddition

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,11 +1,40 @@
 import React from "react";
 import '../ComponentsCSS/about.css'
+import Chart from "chart.js";
 
 export default class About extends React.Component {
+    chartRef = React.createRef();
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+
+        new Chart(myChartRef, {
+            type: "horizontalBar",
+            data: {
+                //Bring in data
+                labels: ["C++", "Javascript", "Python"],
+                datasets: [
+                    {
+                        label: "Core Skills",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+                color: [
+                'red',
+                'blue',
+                'green'
+ ]           }
+        });
+    }
     render() {
         return (
-            <div className="aboutContainer">
-                <h2>About section</h2>
+            <div className="aboutContainer"><canvas id="myChart" width="400" height="400"></canvas>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
             </div>
         )
     }
