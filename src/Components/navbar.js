6a4be323ac1bf8ca5dd836eafa8050caf8dc9625
Oscar Maldonado <oscarmmgg1234@gmail.com>
2020-10-21 20:52:04 -0700

    firebaseAuth

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -4,7 +4,8 @@ import '../ComponentsCSS/navbar.css';
 import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
+
+//import firebaseConfig from './firebaseConfig';
 
 
 export default class NAVBAR extends React.Component {
